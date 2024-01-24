682b29171cd659aa80fc35ac93d66b58e12f8eab
Roman Lavery <romanlavery@Romans-MBP.lan>
2024-01-24 11:11:13 -0700

    added styling to recipe item and home index

--- src/components/recipe/recipe-item.js
+++ src/components/recipe/recipe-item.js
@@ -1,11 +1,16 @@
 import { Link } from "react-router-dom";
+
+import image from '../../assets/images/overtime_image2.jpeg';
 function RecipeItem(props) {
 
     return (
-        <Link to={`/recipe/${props.id}`} className="recipe-item">
+        <Link to={`/recipe/${props.id}`} className="recipe-item" style={{ backgroundImage: `url(${image})`, backgroundSize: 'cover', backgroundPosition: 'center', backgroundRepeat: 'no-repeat' }}>
             <h2 className="recipe-item-title">{props.title}</h2>
-            <p className="recipe-item-description">{props.description}</p>
-            <p className="recipe-item-time">{props.time} min</p>
+
+            <div className="recipe-item-details">
+                <p className="recipe-item-description">{props.description}</p>
+                <p className="recipe-item-time">{props.time} min</p>
+            </div>
         </Link>
     );
 }
